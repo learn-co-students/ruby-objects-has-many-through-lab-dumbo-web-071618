a8b151496c3d01b75ce5872c66737a3c436e5f7f
derickcastillo38 <9derick@gmail.com>
2018-07-21 15:32:26 -0400

    created artist/song/genre domain model, started next model

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+  attr_reader :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    
+  end
+end
