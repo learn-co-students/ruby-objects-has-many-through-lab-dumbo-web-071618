dcb3beedfaab7ba4475973cb988f6814386804c3
derickcastillo38 <9derick@gmail.com>
2018-07-22 12:46:28 -0400

    create doctor/appointment/patient domain model

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,6 +14,18 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-    
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appt|
+      appt.patient
+    end
   end
 end
